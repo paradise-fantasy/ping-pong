74368d3787013c54aceed21305b22a3180c6b0a4
khrall <raymitoro@gmail.com>
2017-01-25 20:31:49 +0100

    Laget en super-naruto intro-skjerm til websiden

--- web/src/App.js
+++ web/src/App.js
@@ -1,18 +1,102 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import ninjaruto_font from './njnaruto.ttf';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to bitch</h2>
+        <div className="App-text-logo">
+          <h1>The KomTek Table-Tennis League</h1>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        <div className="App-scoreboard">
+          <table>
+            <tbody>
+              <tr>
+                <td>1.</td>
+                <td>Raymi</td>
+                <td>1723</td>
+                <td>(17-3)</td>
+              </tr>
+              <tr>
+                <td>2.</td>
+                <td>Håvard</td>
+                <td>1541</td>
+                <td>(11-5)</td>
+              </tr>
+              <tr>
+                <td>3.</td>
+                <td>Frederik</td>
+                <td>1327</td>
+                <td>(10-4)</td>
+              </tr>
+              <tr>
+                <td>1.</td>
+                <td>Raymi</td>
+                <td>1723</td>
+                <td>(17-3)</td>
+              </tr>
+              <tr>
+                <td>2.</td>
+                <td>Håvard</td>
+                <td>1541</td>
+                <td>(11-5)</td>
+              </tr>
+              <tr>
+                <td>3.</td>
+                <td>Frederik</td>
+                <td>1327</td>
+                <td>(10-4)</td>
+              </tr>
+              <tr>
+                <td>1.</td>
+                <td>Raymi</td>
+                <td>1723</td>
+                <td>(17-3)</td>
+              </tr>
+              <tr>
+                <td>2.</td>
+                <td>Håvard</td>
+                <td>1541</td>
+                <td>(11-5)</td>
+              </tr>
+              <tr>
+                <td>3.</td>
+                <td>Frederik</td>
+                <td>1327</td>
+                <td>(10-4)</td>
+              </tr>
+              <tr>
+                <td>1.</td>
+                <td>Raymi</td>
+                <td>1723</td>
+                <td>(17-3)</td>
+              </tr>
+              <tr>
+                <td>2.</td>
+                <td>Håvard</td>
+                <td>1541</td>
+                <td>(11-5)</td>
+              </tr>
+              <tr>
+                <td>3.</td>
+                <td>Frederik</td>
+                <td>1327</td>
+                <td>(10-4)</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+
+        <div className="App-instructions">
+          <marquee>
+            <span>To start a ranked game, swipe each player's card on the RFID-reader one at a time.</span>
+            <span>To start an unranked game, hold both score-buttons for 3+ seconds.</span>
+            <span>To register a new player, click "Register".</span>
+            <span>Thanks to all our supporters. Moms & Dads, bros and hoes &lt;3</span>
+          </marquee>
+        </div>
       </div>
     );
   }
--- web/src/index.js
+++ web/src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
+import background from './background.jpg';
+
+
+
 ReactDOM.render(
   <App />,
   document.getElementById('root')
