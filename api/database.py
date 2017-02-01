a79f5917e471e12fc313ae068b658f378db06751
Haavard Fagervoll <fagervoll@gmail.com>
2017-02-01 17:57:01 +0100

    edit createPlayer

--- api/database.py
+++ api/database.py
@@ -1,28 +1,16 @@
-
 import pymysql.cursors
-
-
 
 class DB:
 
-
     def __init__(self):
-       # try:
         self.connection = pymysql.connect(host='127.0.0.1',
                                             user='testuser',
                                             password='testpass',
                                             db='test',
                                             charset='utf8mb4',
                                             cursorclass=pymysql.cursors.DictCursor)
-        #except mysql.connector.Error as err:
-        #    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-        #        print("Something is wrong with your user name or password")
-        #    elif err.errno == errorcode.ER_BAD_DB_ERROR:
-        #        print("Database does not exist")
-        #    else:
-        #        print(err)
 
-    def createPlayer(self, name, cardid, profile_picture):
+    def createPlayer(self, name, cardid, surname="", profile_picture=""):
         try:
             connection = self.connection
             with connection.cursor() as cursor:
@@ -30,9 +18,8 @@ class DB:
                 cursor.execute(sql, (name, cardid, profile_picture))
 
             connection.commit()
-
         finally:
-            print "finally"
+            return True;
     def getPlayerFromPlayerId(self, playerid):
         try:
             connection = self.connection
@@ -41,9 +28,8 @@ class DB:
                 cursor.execute(sql, (playerid,))
 
                 result = cursor.fetchone()
-                print result
         finally:
-            print "finally"
+            return result
 
     def getPlayerFromCardId(self, cardid):
         try:
@@ -51,11 +37,10 @@ class DB:
             with connection.cursor() as cursor:
                 sql = "SELECT * FROM `player` WHERE `id`=%s"
                 cursor.execute(sql, (playerid,))
-
                 result = cursor.fetchone()
-                print result
+
         finally:
-            print "finally"
+            return result
 
     def close(self):
         self.connection.close()
