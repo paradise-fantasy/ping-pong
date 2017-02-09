1ef2ee3907aa49d9bb9c2cc884675b956f47bd88
Haavard Fagervoll <fagervoll@gmail.com>
2017-02-09 15:41:31 +0100

    indent error

--- game/read_card.py
+++ game/read_card.py
@@ -10,8 +10,8 @@ def read_card(hardware):
     	proc = subprocess.Popen("explorenfc-basic", stdout=subprocess.PIPE, shell=True)
     	(rfid, error) = proc.communicate()
         if not error:
-        	rfid_array = rfid.split(":")
-        	current_scan  = [rfid_array[-2][-3:].strip(), rfid_array[-1].strip(), time.time()]
+            rfid_array = rfid.split(":")
+            current_scan  = [rfid_array[-2][-3:].strip(), rfid_array[-1].strip(), time.time()]
             action = Action( Action.CARD_SWIPE, current_scan[1] )
             if ( last_scan[1] == current_scan[1] ): # Same card as last scan?
                 if ( current_scan[2] < last_scan[2] + delay ): # Avoid spamming same card
