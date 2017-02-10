ab8ce6493ef95a3d2f3835c1e08820b7058ae38a
Ping Pong <pingpong>
2017-02-10 11:31:45 +0000

    button definition, a little slow to catch events, seems like combination of sleep and system delay

--- game/button_event.py
+++ game/button_event.py
@@ -4,21 +4,24 @@ from RPi import GPIO
 from time import sleep
 from actions import Action
 
-def button_event(hardware)
+def button_event(hardware):
     GPIO.setmode(GPIO.BOARD)
 
     GPIO.setup(37,GPIO.IN, pull_up_down=GPIO.PUD_UP)
     GPIO.setup(40,GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
     while hardware.running:
+        action = ''
         greenBtn = GPIO.input(37)
         redBtn = GPIO.input(40)
         while (greenBtn):
             greenBtn = GPIO.input(37)
             action = Action( Action.BUTTON_1_PRESS )
-            sleep(0.25)
-        while (greenBtn):
+            sleep(.25)
+        while (redBtn):
             redBtn = GPIO.input(40)
             action = Action( Action.BUTTON_2_PRESS )
-            sleep(0.25)
-        hardware.insert_action(action)
+            sleep(.25)
+        if action != '':
+            hardware.insert_action(action)
+
