7c13b5e51030bd0ff9d3ad36196707822bf9e760
Ping Pong <pingpong>
2017-02-13 22:19:59 +0000

    removed printing

--- game/button_event.py
+++ game/button_event.py
@@ -4,19 +4,18 @@ from RPi import GPIO
 from time import time, sleep
 from actions import Action
 
-GPIO.setmode(GPIO.BOARD)
-
-GPIO.setup(37,GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.setup(40,GPIO.IN, pull_up_down=GPIO.PUD_UP)
-
-
-LONG_PRESS_THRESHOLD = 3
-HOLD_EVENTS = False
-
-last_btn1 = 0
-last_btn2 = 0
-
 def button_event(hardware):
+    GPIO.setmode(GPIO.BOARD)
+
+    GPIO.setup(37,GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    GPIO.setup(40,GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+    LONG_PRESS_THRESHOLD = 3
+    HOLD_EVENTS = False
+
+    last_btn1 = 0
+    last_btn2 = 0
+
     while hardware.running:
         btn1 = GPIO.input(37) # 0 if up, 1 if down
         btn2 = GPIO.input(40) # 0 if up, 1 if down
@@ -26,32 +25,32 @@ def button_event(hardware):
         # print "states " + btn1 + ", " + btn2
 
         if btn1 and not last_btn1:
-            print "green down"
+            #print "green down"
             last_btn1_event = time()
 
         if btn2 and not last_btn2:
-            print "red down"
+            #print "red down"
             last_btn2_event = time()
     
         if last_btn1 and not btn1 and not HOLD_EVENTS:
             delta = time() - last_btn1_event
             if delta > LONG_PRESS_THRESHOLD:
-                print "green long press"
+                #print "green long press"
                 action = Action( Action.BUTTON_2_LONG_PRESS )
                 hardware.insert_action(action)
             else:
-                print "green press"
+                #print "green press"
                 action = Action( Action.BUTTON_2_PRESS )
                 hardware.insert_action(action)
 
         if last_btn2 and not btn2 and not HOLD_EVENTS:
             delta = time() - last_btn2_event
             if delta > LONG_PRESS_THRESHOLD:
-                print "red long press"
+                #print "red long press"
                 action = Action( Action.BUTTON_1_LONG_PRESS )
                 hardware.insert_action(action)
             else:
-                print "red press"
+                #print "red press"
                 action = Action( Action.BUTTON_1_PRESS )
                 hardware.insert_action(action)
     
@@ -59,10 +58,8 @@ def button_event(hardware):
             delta1 = time() - last_btn1_event
             delta2 = time() - last_btn2_event
             if delta1 > LONG_PRESS_THRESHOLD and delta2 > LONG_PRESS_THRESHOLD:
-                if HOLD_EVENTS:
-                    print "holding.."
-                else:
-                    print "both long press"
+                if not HOLD_EVENTS:
+                    #print "both long press"
                     action = Action( Action.BOTH_BUTTONS_LONG_PRESS )
                     hardware.insert_action(action)
                     HOLD_EVENTS = True
