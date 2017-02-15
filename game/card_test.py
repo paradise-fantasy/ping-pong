fa8f77f226424866497adad700c74f208b8001a1
Ping Pong <pingpong>
2017-02-15 09:59:30 +0000

    Works with card reader, but main.py yields segmentation fault

--- game/card_test.py
+++ game/card_test.py
@@ -1,28 +1,19 @@
-import subprocess
-from actions import Action
+import nxppy
 import time
 
-def read_card(hardware):
-    last_scan = ['','', time.time()]
+mifare = nxppy.Mifare()
+
+def read_card():
     delay = 5
 
     while True:
-    	proc = subprocess.Popen("explorenfc-basic", stdout=subprocess.PIPE, shell=True)
-    	(rfid, error) = proc.communicate()
-        if not error:
-            rfid_array = rfid.split(":")
-            current_scan  = [rfid_array[-2][-3:].strip(), rfid_array[-1].strip(), time.time()]
-            action = Action( Action.CARD_SWIPE, current_scan[1] )
-            if ( last_scan[1] == current_scan[1] ): # Same card as last scan?
-                if ( current_scan[2] < last_scan[2] + delay ): # Avoid spamming same card
-                    pass
-                else:
-                    hardware.insert_action(action)
-                    last_scan = list(current_scan)
-            else: # New card
-                hardware.insert_action(action)
-                last_scan = list(current_scan)
-        else:
-            print str(error)
+        try:
+            uid = mifare.select()
+            print uid
+            time.sleep(delay)
+        except nxppy.SelectError:
+            # SelectError is raised if no card is in the field.
+            pass
+        time.sleep(0.2)
 
-read_card(None)
+read_card()
--- game/read_card.py
+++ game/read_card.py
@@ -1,26 +1,19 @@
-import subprocess
+import nxppy
 from actions import Action
 import time
 
+mifare = nxppy.Mifare()
+
 def read_card(hardware):
-    last_scan = ['','', time.time()]
-    delay = 5
+    delay = 2 # In order not to scan same card twice
 
     while hardware.running:
-    	proc = subprocess.Popen("explorenfc-basic", stdout=subprocess.PIPE, shell=True)
-    	(rfid, error) = proc.communicate()
-        if not error:
-            rfid_array = rfid.split(":")
-            current_scan  = [rfid_array[-2][-3:].strip(), rfid_array[-1].strip(), time.time()]
-            action = Action( Action.CARD_SWIPE, current_scan[1] )
-            if ( last_scan[1] == current_scan[1] ): # Same card as last scan?
-                if ( current_scan[2] < last_scan[2] + delay ): # Avoid spamming same card
-                    pass
-                else:
-                    hardware.insert_action(action)
-                    last_scan = list(current_scan)
-            else: # New card
-                hardware.insert_action(action)
-                last_scan = list(current_scan)
-        else:
-            print str(error)
+        try:
+            uid = mifare.select()
+            action = Action( Action.CARD_SWIPE, uid )
+            hardware.insert_action(action)
+            time.sleep(delay)
+        except nxppy.SelectError:
+            # SelectError is raised if no card is in the field.
+            pass
+        time.sleep(0.2)
